a621129bc36d8894dc1e3c4ec564aeb535b05041
Dylan Spencer <dspencer@zipmac010.local>
2016-01-08 16:03:24 -0500

    Add match combiner

--- lib/morse_removal.ex
+++ lib/morse_removal.ex
@@ -2,11 +2,32 @@ defmodule MorseRemoval do
   @moduledoc
   def find_matches( string, matcher ), do: do_find_matches( string, matcher, 0, [])
 
-  defp do_find_matches( <<>>, matcher, depth, results ), do: Enum.reverse(results)
+  defp do_find_matches( <<>>, _matcher, _depth, results ), do: Enum.reverse(results)
   defp do_find_matches( << head::binary-size(1), tail::binary >>, matcher, depth, results ) when head == matcher do
     do_find_matches( tail, matcher, depth + 1, [ depth | results ] )
   end
   defp do_find_matches( << head::binary-size(1), tail::binary >>, matcher, depth, results ) when head != matcher do
     do_find_matches( tail, matcher, depth + 1, results )
+  end
+
+  def add_indices( new_list, [] ), do: Enum.map( new_list, fn(n) -> [n] end )
+  def add_indices( new_list, combined_list ), do: do_add_indices( new_list, combined_list, [] )
+
+  defp do_add_indices( [], _combined_list, acc), do: acc
+  defp do_add_indices( [head | tail], combined_list, acc ) do
+    do_add_indices(tail, combined_list, acc ++ add_index(head, combined_list))
+  end
+
+  defp add_index( element, combined_list ) do
+    do_add_index( element, combined_list, [] )
+  end
+
+  defp do_add_index( _element, [], results ), do: results
+  defp do_add_index( element, [head | tail], results ) do
+    if element > List.last(head) do
+      do_add_index( element, tail, results ++ [ head ++ [ element ] ] )
+    else
+      do_add_index( element, tail, results )
+    end
   end
 end
--- test/morse_removal_test.exs
+++ test/morse_removal_test.exs
@@ -8,4 +8,11 @@ defmodule MorseRemovalTest do
     assert MorseRemoval.find_matches( "*.*.*.", "*") == [0,2,4]
   end
 
+  test "combine indices lists" do
+    assert MorseRemoval.add_indices( [], [] ) == []
+    assert MorseRemoval.add_indices( [0,2,4], [] ) == [ [0], [2], [4] ]
+    assert MorseRemoval.add_indices( [1,2,3], [ [0] ] ) == [ [0,1], [0,2], [0,3] ]
+    assert MorseRemoval.add_indices( [5], [ [0], [2], [4] ] ) == [ [0,5], [2,5], [4,5] ]
+    assert MorseRemoval.add_indices( [1,4], [ [0], [2], [4] ] ) == [ [0,1], [0,4], [2,4] ]
+  end
 end
